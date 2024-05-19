35ec27edce5ec84e619152b924c22816c89a4937
huy.nguyen <yasuo01665601635@gmail.com>
2024-05-19 21:26:41 +0700

    Bai tap chuong 4 slide 115

---
+++ Lab4/src/baitapslide115.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class baitapslide115 {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashMap<String, student> danhSachSinhVien = new HashMap<>();
+
+        System.out.println("Nhap so luong sinh vien : ");
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            System.out.println("Nhap thong tin cho sinh vien thu  " + (i + 1) + ":");
+            System.out.println("MSV: ");
+            String maSV = sc.nextLine();
+            System.out.println("Name: ");
+            String hoTen = sc.nextLine();
+            System.out.println("Class: ");
+            String lop = sc.nextLine();
+            danhSachSinhVien.put(maSV, new student(maSV, hoTen, lop));
+        }
+
+        System.out.println("Nhap ten class can tim : ");
+        String lopCanTim = sc.nextLine();
+        System.out.println("Sinh vien thuoc lop " + lopCanTim + ":");
+        for (student sv : danhSachSinhVien.values()) {
+            if (sv.getLop().equalsIgnoreCase(lopCanTim)) {
+                System.out.println("MSV : " + sv.getMaSV() + ", Name : " + sv.getHoTen() + ", Class : " + sv.getLop());
+            }
+        }
+
+        System.out.print("Nhap MSV can tim : ");
+        String maSVCantim = sc.nextLine();
+        student sinhVien = danhSachSinhVien.get(maSVCantim);
+        if (sinhVien != null) {
+            System.out.println("Sinh vien co MSV " + maSVCantim + ":");
+            System.out.println("Name: " + sinhVien.getHoTen() + ", Class: " + sinhVien.getLop());
+        } else {
+            System.out.println("Khong tim thay " + maSVCantim);
+        }
+
+        sc.close();
+    }
+}
---
+++ Lab4/src/student.java
@@ -0,0 +1,24 @@
+public class student {
+        public String maSV;
+        public String hoTen;
+        public String lop;
+    
+        public student (String maSV, String hoTen, String lop) {
+            this.maSV = maSV;
+            this.hoTen = hoTen;
+            this.lop = lop;
+        }
+    
+        public String getMaSV() {
+            return maSV;
+        }
+    
+        public String getHoTen() {
+            return hoTen;
+        }
+    
+        public String getLop() {
+            return lop;
+        }
+    }
+    
