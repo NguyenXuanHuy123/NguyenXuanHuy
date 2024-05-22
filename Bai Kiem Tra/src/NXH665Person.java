09725efc4a30db1bd1ad6df318452de0e054f723
huy.nguyen <yasuo01665601635@gmail.com>
2024-05-22 09:45:17 +0700

    NXH665-Cau2

--- Bai Kiem Tra/src/NXH665Person.java
+++ Bai Kiem Tra/src/NXH665Person.java
@@ -1,8 +1,27 @@
+
+import java.util.Scanner;
+
 public class NXH665Person {
     public static void main(String[] args) {
-        
     }
     String name;
     int age;
     Long ID;
+    public void Nhap(){
+    Scanner sc = new Scanner(System.in);
+        System.out.println("nhap ten : ");
+        name = sc.nextLine();
+        System.out.println("nhap tuoi :" );
+        age = sc.nextInt();
+        System.out.println("nhap ID ");
+        ID = sc.nextLong();
+    }
+    public void Xuat(){
+        System.out.println("Ho va ten la :" + name);
+        System.out.println("tuoi : " + age);
+        System.out.println("So ID " + ID);
+    }
+
+    
+    
 }
