9d4b9bc0a0998efcc97312f20d7622a1cd6fb36a
huy.nguyen <yasuo01665601635@gmail.com>
2024-06-07 18:03:49 +0700

    Bai thuc hanh chuong 5 slide 12

---
+++ BaiThucHanh/src/Baitapchuong5/slide12.java
@@ -0,0 +1,17 @@
+package Baitapchuong5;
+
+public class slide12 {
+    public static void main(String[] args) {
+        try{
+            int data = 123;
+            if(data % 2 !=0){
+                System.out.println(data+" is odd number ");
+            }
+        }catch(ArithmeticException e){
+            System.out.println(e);
+        }finally{
+            System.out.println("Finally block is always executed");
+        }
+        System.out.println("Rest of the code ");
+    }
+}
